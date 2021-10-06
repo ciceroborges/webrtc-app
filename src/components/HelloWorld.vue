e58d128feadded91f47f1e6b66a4f10b19e053c6
Cícero Borges <ciceropborges@gmail.com>
2021-10-05 21:58:44 -0300

    Permissao de uso e listagem de dispositivos

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,6 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <label>Câmeras disponíveis: </label><select id="camera-list"/>
   </div>
 </template>
 
@@ -35,6 +9,52 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  mounted() {
+    // pedir permissao para usar os dispositivos de audio e video
+    try {
+        const stream = this.openMediaDevices({'video':true,'audio':true});
+        console.log('Got MediaStream:', stream);
+    } catch(error) {
+        console.error('Error accessing media devices.', error);
+    }
+
+    // exibe e armazena a lista de dispositivos conectados
+    const videoCameras = this.getConnectedDevices('videoinput')
+          .then((res) => this.updateCameraList(res));
+        console.log('Cameras found:', videoCameras);
+
+    // escuta mudanças nos dispositivos e atualiza a lista
+    navigator.mediaDevices.addEventListener('devicechange', event => {
+        const newCameraList = this.getConnectedDevices('video')
+          .then((res) => this.updateCameraList(res));
+        console.log(newCameraList);
+        console.log(event);
+    });
+  },
+  methods: {
+    // acessar cameras ou microfones conectados no pc / celular
+    async openMediaDevices(constraints) {
+      return await navigator.mediaDevices.getUserMedia(constraints);
+    },
+    // verificar todas as câmeras e microfones conectados
+    async getConnectedDevices(type) {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      return devices.filter(device => device.kind === type)
+    },
+    // Atualiza a lista de cameras
+    updateCameraList(cameras) {
+        const listElement = document.getElementById('camera-list');
+        listElement.innerHTML = '';
+        console.log(cameras);
+        cameras.map(camera => {
+            const cameraOption = document.createElement('option');
+            cameraOption.label = camera.label;
+            cameraOption.value = camera.deviceId;
+            listElement.add(cameraOption)
+        });
+        
+    },
   }
 }
 </script>
